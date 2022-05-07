070c9bc31f57356485512252bd5fb6556dec897b
Vladimir Fedorchenko <onevovan@softkitit.com>
2022-05-08 01:22:32 +0300

    add show server data

--- src/pages/using-ssr.js
+++ src/pages/using-ssr.js
@@ -4,13 +4,29 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const UsingSSR = ({ serverData }) => {
+const UsingSSR = ({ serverData, ...rest }) => {
+  console.log('serverData: ', serverData);
+  console.log('rest: ', rest);
+
   return (
     <Layout>
       <Seo title="Using SSR" />
       <h1>
         This page is <b>rendered server-side</b>
       </h1>
+      {serverData.data.map((item, k) => {
+        return <div key={k}>
+          <code>
+            {JSON.stringify(item, null, 2)}
+          </code>
+          <br />
+          <br />
+        </div>
+
+      })}
+      <br />
+      <br />
+      <br />
       <p>
         This page is rendered server side every time the page is requested.
         Reload it to see a(nother) random photo from{" "}
@@ -37,7 +53,7 @@ export default UsingSSR
 
 export async function getServerData() {
   try {
-    const res = await fetch(`https://dog.ceo/api/breed/shiba/images/random`)
+    const res = await fetch(`http://localhost:1337/api/pages`)
     if (!res.ok) {
       throw new Error(`Response failed`)
     }
